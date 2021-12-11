f08232ce5ab895959a9af259f4d4ba458c3758fb
K K Lin <klin@math.arizona.edu>
2021-12-10 23:06:05 -0700

    more copyleft

--- Utils/MF_Free_LIFEst_MultiCell_L6.m
+++ Utils/MF_Free_LIFEst_MultiCell_L6.m
@@ -1,3 +1,30 @@
+% MF_Free_LIFEst_MultiCell_L6.m
+
+% This file is part of NYU_DIMF_PLOSCB, the code
+% accompanying the paper "A data-informed mean-field
+% approach to mapping of cortical parameter landscapes," to
+% appear in PLoS Computational Biology
+% (https://arxiv.org/abs/2110.12286).
+% 
+% Copyright (C) 2021 by Zhuo-Cheng Xiao <zx555@nyu.edu>
+% 
+% This program is free software; you can redistribute it
+% and/or modify it under the terms of the GNU General Public
+% License as published by the Free Software Foundation;
+% either version 2 of the License, or (at your option) any
+% later version.
+% 
+% This program is distributed in the hope that it will be
+% useful, but WITHOUT ANY WARRANTY; without even the implied
+% warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+% PURPOSE.  See the GNU General Public License for more
+% details.
+% 
+% You should have received a copy of the GNU General Public
+% License along with this program; if not, write to the Free
+% Software Foundation, Inc., 51 Franklin Street, Fifth
+% Floor, Boston, MA 02110-1301 USA.
+
 %% single cell simulation: to collect mean V (and firing rates)
 % Input: NeuronType           'e' or 'i'
 %        InitialStates        V and all Gs
--- Utils/MeanFieldEst_BkGd_L6_OneStep.m
+++ Utils/MeanFieldEst_BkGd_L6_OneStep.m
@@ -1,3 +1,30 @@
+% MeanFieldEst_BkGd_L6_OneStep.m
+
+% This file is part of NYU_DIMF_PLOSCB, the code
+% accompanying the paper "A data-informed mean-field
+% approach to mapping of cortical parameter landscapes," to
+% appear in PLoS Computational Biology
+% (https://arxiv.org/abs/2110.12286).
+% 
+% Copyright (C) 2021 by Zhuo-Cheng Xiao <zx555@nyu.edu>
+% 
+% This program is free software; you can redistribute it
+% and/or modify it under the terms of the GNU General Public
+% License as published by the Free Software Foundation;
+% either version 2 of the License, or (at your option) any
+% later version.
+% 
+% This program is distributed in the hope that it will be
+% useful, but WITHOUT ANY WARRANTY; without even the implied
+% warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+% PURPOSE.  See the GNU General Public License for more
+% details.
+% 
+% You should have received a copy of the GNU General Public
+% License along with this program; if not, write to the Free
+% Software Foundation, Inc., 51 Franklin Street, Fifth
+% Floor, Boston, MA 02110-1301 USA.
+
 %% mean-field w/o ref. We use parameters and mean Vs to estimate firing rates
 % Input: C_EE,C_EI,C_IE,C_II connectivity matrices
 %        N_EE,N_EI,N_IE,N_II number of presynaptic neurons
